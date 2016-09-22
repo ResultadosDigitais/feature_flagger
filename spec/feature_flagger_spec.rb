d35a9e581576ffce1d9511adf5d00c0e61f69e32
Luiz Cezer <lccezinha@gmail.com>
2016-09-22 10:30:01 -0300

    Rename rollout_spec.rb file for feature_flagger_spec.rb
    The old rollout_spec.rb file was named this way because the old gem's name.
    
    Now the gem's name has changed, this file name must be updated.

--- spec/feature_flagger_spec.rb
+++ spec/feature_flagger_spec.rb
@@ -21,24 +21,4 @@ RSpec.describe FeatureFlagger do
       expect(control.storage).to be_a(FeatureFlagger::Storage::Redis)
     end
   end
-
-  describe '.storage' do
-    context 'no storage set' do
-      it 'returns a Redis storage by default' do
-        FeatureFlagger.config.storage = nil
-        expect(FeatureFlagger.config.storage).to be_a(FeatureFlagger::Storage::Redis)
-      end
-    end
-
-    context 'storage set' do
-      let(:storage) { double('storage') }
-
-      it 'returns storage' do
-        FeatureFlagger.configure do |config|
-          config.storage = storage
-        end
-        expect(FeatureFlagger.config.storage).to eq storage
-      end
-    end
-  end
 end
