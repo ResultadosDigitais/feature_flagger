57896e59b81bcd681670fe49bff09063a2067336
Marcelo Uemura <marcelo.uemura@resultadosdigitiais.com.br>
2017-09-03 15:49:21 -0300

    Bug fix: it was not possible to call FeatureFlagger.configure more than once since FeatureFlagger.control was memoizing the storage passed in the first call. This made testing scenarios particularly harder because it was not possible to override the FeatureFlagger Redis storage for just a single call

--- spec/feature_flagger_spec.rb
+++ spec/feature_flagger_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 RSpec.describe FeatureFlagger do
   describe '.configure' do
-    let(:redis)   { double('redis') }
     let(:storage) { double('storage') }
+    let(:other_storage) { double('other_storage') }
 
     before do
       FeatureFlagger.configure do |config|
@@ -12,10 +12,25 @@ RSpec.describe FeatureFlagger do
     end
 
     it { expect(FeatureFlagger.config.storage).to eq storage }
+
+    it 'Calling configure with a new storage must change control.storage' do
+      FeatureFlagger.configure do |config|
+        config.storage = other_storage
+      end
+
+      expect(FeatureFlagger.config.storage).to eq other_storage
+      expect(FeatureFlagger.control.storage).to eq other_storage
+    end
   end
 
   describe '.control' do
     let(:control) { FeatureFlagger.control }
+
+    before do
+      FeatureFlagger.configure do |config|
+      end
+    end
+
     it 'initializes a Control with redis storage' do
       expect(control).to be_a(FeatureFlagger::Control)
       expect(control.storage).to be_a(FeatureFlagger::Storage::Redis)
