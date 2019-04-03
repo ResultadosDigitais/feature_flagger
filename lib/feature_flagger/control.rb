d3513ebc2aa17664cb7bbf9332e35291c5416078
matheusteixeira <matheusteixeira01@live.com>
2019-04-03 14:14:24 -0300

    Create methods add all and remove all

--- lib/feature_flagger/control.rb
+++ lib/feature_flagger/control.rb
@@ -17,7 +17,7 @@ module FeatureFlagger
     end
 
     def release_to_all(feature_key)
-      @storage.add(RELEASED_FEATURES, feature_key)
+      @storage.add_all(RELEASED_FEATURES, feature_key)
     end
 
     def unrelease(feature_key, resource_id)
@@ -25,7 +25,7 @@ module FeatureFlagger
     end
 
     def unrelease_to_all(feature_key)
-      @storage.remove(RELEASED_FEATURES, feature_key)
+      @storage.remove_all(RELEASED_FEATURES, feature_key)
     end
 
     def resource_ids(feature_key)
