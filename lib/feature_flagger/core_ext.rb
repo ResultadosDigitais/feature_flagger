cd50523fe6068cc62f43dfc67488b8eb2238ca6a
Nando Sousa <nandosousafr@gmail.com>
2020-03-05 15:57:50 -0300

    Add rakes to release,unrelease features.

---
+++ lib/feature_flagger/core_ext.rb
@@ -0,0 +1,18 @@
+begin
+  require 'active_support/core_ext/string/inflections'
+rescue LoadError
+  unless ''.respond_to?(:constantize)
+    class String
+      def constantize
+        names = split('::')
+        names.shift if names.empty? || names.first.empty?
+
+        constant = Object
+        names.each do |name|
+          constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+        end
+        constant
+      end
+    end
+  end
+end
