ba0fca83de47d020c2ca7ad65b043813f76d3b12
André Luis Leal Cardoso Junior <andrehjr@gmail.com>
2021-06-30 17:30:43 -0300

    Update CHANGELOG

--- lib/feature_flagger/configuration.rb
+++ lib/feature_flagger/configuration.rb
@@ -12,9 +12,8 @@ module FeatureFlagger
     def cache_store=(cache_store)
       raise ArgumentError, "Cache is only support when used with ActiveSupport" unless defined?(ActiveSupport)
 
-      if cache_store
-        @cache_store = ActiveSupport::Cache.lookup_store(*cache_store)
-      end
+      cache_store = :null_store if cache_store.nil?
+      @cache_store = ActiveSupport::Cache.lookup_store(*cache_store)
     end
 
     def info
