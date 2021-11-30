e3e4ac8da8d68eac85570d57d98393d14ea4f0da
michele clain <michele.thays@gmail.com>
2021-11-30 12:41:44 -0300

    add-option-with-yaml-file

---
+++ lib/feature_flagger/manifest_sources/with_yaml_file.rb
@@ -0,0 +1,14 @@
+module FeatureFlagger
+  module ManifestSources
+    class WithYamlFile
+      def initialize(yaml_path = nil)
+        @yaml_path = yaml_path
+        @yaml_path ||= "#{Rails.root}/config/rollout.yml" if defined?(Rails)
+      end
+
+      def resolved_info
+        @resolved_info ||= ::YAML.load_file(@yaml_path) if @yaml_path
+      end
+    end
+  end
+end
